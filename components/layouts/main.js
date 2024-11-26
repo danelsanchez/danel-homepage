6a1b8f435cdab88dc5e00c24269d58ef372cf670
mrboat <mr.boat@gmx.com>
2024-11-26 15:53:15 +0200

    Version 0.47

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -12,10 +12,10 @@ const Main = ({ children, router }) => {
         <meta name="author" content="Danel Sánchez" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="realmrboat" />
+        <meta name="twitter:title" content="Danel Sánchez" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
+        <meta name="twitter:site" content="@danelsanchez" />
+        <meta name="twitter:creator" content="@danelsanchez" />
         <meta name="twitter:image" content="/card.png" />
         <meta property="og:site_name" content="Danel Sánchez" />
         <meta name="og:title" content="Danel Sánchez" />
